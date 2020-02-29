a6c9a7b2de63f66cfb657b579fafb5588069eced
SarahWu99 <swu75@smith.edu>
2020-02-28 23:23:20 -0500

    Almost finished challenge methods

--- src/main/java/edu/smith/cs/csc212/adtr/Challenges.java
+++ src/main/java/edu/smith/cs/csc212/adtr/Challenges.java
@@ -12,15 +12,28 @@ public class Challenges {
 		return output;
 	}
 	
+	//Yilin
 	public static SetADT<String> intersection(SetADT<String> left, SetADT<String> right) {
-		SetADT<String> output = new JavaSet<>();
-		
-		return output;
-	}
-	
+	  SetADT<String> output = new JavaSet<>();
+	  for (String element: left) {
+	   if (right.contains(element)) {
+	    output.insert(element);
+	   } 
+	  }
+	  return output;
+	 }
+	 
+	 //Yilin
 	public static MapADT<String, Integer> wordCount(ListADT<String> words) {
-		MapADT<String, Integer> output = new JavaMap<>();
-		
-		return output;
+	  MapADT<String, Integer> output = new JavaMap<>();
+	  for (String word: words) {
+	   System.out.println(words.size() + word + output.get(word)+ "\n");
+	   if (output.get(word) == null) { // appear for the first time
+	    output.put(word, 1);
+	   } else if (output.get(word) >= 1){ // value += 1
+	    output.put(word,output.get(word)+1);
+	   }
+	  }
+	  return output;
 	}
 }
